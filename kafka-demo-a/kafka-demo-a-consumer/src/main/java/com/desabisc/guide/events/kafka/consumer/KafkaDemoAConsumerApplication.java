c02b276ad0310469db72f0357a017b350892588f
desabi <desabi@live.com>
2025-04-07 18:35:41 -0600

    feat: kafka-demo-a, consumer

---
+++ kafka-demo-a/kafka-demo-a-consumer/src/main/java/com/desabisc/guide/events/kafka/consumer/KafkaDemoAConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.events.kafka.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoAConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoAConsumerApplication.class, args);
+	}
+
+}
---
+++ kafka-demo-a/kafka-demo-a-consumer/src/test/java/com/desabisc/guide/events/kafka/consumer/KafkaDemoAConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.events.kafka.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoAConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
