58106854bde4ab1346c3d627920005474a3d7d54
desabi <desabi@live.com>
2025-04-07 18:08:18 -0600

    feat: kafka-demo-a, base project

---
+++ kafka-demo-a/src/main/java/com/desabisc/guide/events/kafka/KafkaDemoAApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.events.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoAApplication.class, args);
+	}
+
+}
