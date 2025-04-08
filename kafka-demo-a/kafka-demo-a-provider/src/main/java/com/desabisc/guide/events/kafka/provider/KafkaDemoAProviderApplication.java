f1ee0a8ad220d882ed3438ed913327b605b42ab5
desabi <desabi@live.com>
2025-04-07 18:30:17 -0600

    feat: kafka-demo-a, provider

---
+++ kafka-demo-a/kafka-demo-a-provider/src/main/java/com/desabisc/guide/events/kafka/provider/KafkaDemoAProviderApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.events.kafka.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaDemoAProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaDemoAProviderApplication.class, args);
+	}
+
+}
---
+++ kafka-demo-a/kafka-demo-a-provider/src/test/java/com/desabisc/guide/events/kafka/provider/KafkaDemoAProviderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.guide.events.kafka.provider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaDemoAProviderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
