bdc3db5284ba2790b80be4100f47d82761b46bdd
desabi <desabi@live.com>
2025-07-21 19:14:40 -0600

    feat: avro ega, base project

---
+++ kafka-avro-ega/src/test/java/com/desabi/guide/kafka/KafkaAvroEgaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.guide.kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaAvroEgaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
