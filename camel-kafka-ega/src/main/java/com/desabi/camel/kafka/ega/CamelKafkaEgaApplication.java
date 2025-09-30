eaf3fa7d0b116026e5fb4983e88dad6a1af1c36a
desabi <desabi@live.com>
2025-09-29 20:47:15 -0600

    feat: camel-kafka-ega, base project

---
+++ camel-kafka-ega/src/main/java/com/desabi/camel/kafka/ega/CamelKafkaEgaApplication.java
@@ -0,0 +1,13 @@
+package com.desabi.camel.kafka.ega;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamelKafkaEgaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelKafkaEgaApplication.class, args);
+	}
+
+}
