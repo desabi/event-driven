f1158187d1ecbbeea6628bbea2d5fe5fd5984008
desabi <desabi@live.com>
2025-09-30 19:02:03 -0600

    feat: camel-kafka-tests, base project

---
+++ camel-kafka-tests/src/test/java/com/desabi/camel/kafka/tests/CamelKafkaTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabi.camel.kafka.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CamelKafkaTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
